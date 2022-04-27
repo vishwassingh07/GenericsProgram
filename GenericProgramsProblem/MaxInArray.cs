21e00bac98c84205ea13c8dc8d963b8f3d137ded
Vishwas <baghel.vishwas0@gmail.com>
2022-04-28 01:28:26 +0530

    [Vishwas]add: Find Max Number In An Array

---
+++ GenericProgramsProblem/MaxInArray.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericProgramsProblem
+{
+    public class MaxInArray
+    {
+        public void MaxNumber(int[] intArray)
+        {
+            foreach(int element in intArray)
+            {
+                Console.WriteLine(element+" ");
+            }
+            Console.WriteLine(" ");
+            Array.Sort(intArray);
+            int length = intArray.Length;
+            Console.WriteLine("Maximum Number In This Array Is :" + intArray[length - 1]);
+        }
+    }
+}
---
+++ GenericProgramsProblem/Program.cs
@@ -0,0 +1,14 @@
+﻿using GenericProgramsProblem;
+using System;
+namespace GenericProgramProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] intArr = {143,432,543,6944,5555};
+            MaxInArray array = new MaxInArray();
+            array.MaxNumber(intArr); 
+        }
+    }
+}
